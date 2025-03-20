ed6b7b866e0576795c670c4903a23bd913f8cec6
ShimanshuChauhan <shivanshuchauhan699@gmail.com>
2025-03-20 10:39:22 +0530

    chore: database connection

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 process.on('uncaughtException', (err) => {
@@ -9,6 +10,15 @@ process.on('uncaughtException', (err) => {
 dotenv.config({ path: './config.env' });
 import app from './app.js';
 
+if (!process.env.DATABASE || !process.env.DATABASE_PASSWORD) {
+  throw new Error('DATABASE or DATABASE_PASSWORD environment variable is not defined.');
+}
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
+mongoose.connect(DB, clientOptions).then(() => console.log('DB connection successful!'));
+
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
@@ -17,6 +27,7 @@ const server = app.listen(port, () => {
 process.on('unhandledRejection', (err) => {
   console.log('UNHANDLED REJECTION! 💥 Shutting down...');
   console.log(err.name, err.message);
+  console.log(err);
   server.close(() => {
     process.exit(1);
   });
