e677848bc50a148f25db69086906ad4011ea4659
ShimanshuChauhan <shivanshuchauhan699@gmail.com>
2025-03-26 16:51:01 +0530

    chore: added produciton error handler

--- backend/controllers/errorController.js
+++ backend/controllers/errorController.js
@@ -1,4 +1,27 @@
 import AppError from '../utils/appError.js';
+import dotenv from 'dotenv';
+dotenv.config({ path: './config.env' });
+
+const handleOAuthError = (err) => {
+  const message = 'Access denied. Please try again.';
+  return new AppError(message, 403);
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+  } else {
+    console.log('Error 💥', err);
+
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!'
+    });
+  }
+};
 
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
@@ -15,6 +38,15 @@ export default function (err, req, res, next) {
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
+    console.log(err.name);
     sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+
+    if (err.message === 'access_denied') {
+      error = handleOAuthError(error);
+    }
+
+    sendErrorProd(error, res);
   }
 };
